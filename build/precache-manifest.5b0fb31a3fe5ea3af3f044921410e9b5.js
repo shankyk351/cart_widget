910c8e40292d80ba743dbb1504575474d9699955
hgupta10 <himanshu.h.gupta@capgemini.com>
2019-10-16 01:34:43 +0530

    github pages deployment code added

---
+++ build/precache-manifest.5b0fb31a3fe5ea3af3f044921410e9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab0d8fd8f5230875ccbff095d9f9f8d",
+    "url": "/cart_widget/index.html"
+  },
+  {
+    "revision": "bd5eae1599042e33f65f",
+    "url": "/cart_widget/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f002b50c7c5f98ef34c6",
+    "url": "/cart_widget/static/css/main.12246435.chunk.css"
+  },
+  {
+    "revision": "bd5eae1599042e33f65f",
+    "url": "/cart_widget/static/js/2.d7cb98a0.chunk.js"
+  },
+  {
+    "revision": "f002b50c7c5f98ef34c6",
+    "url": "/cart_widget/static/js/main.21355319.chunk.js"
+  },
+  {
+    "revision": "68f743b7735fc31ad078",
+    "url": "/cart_widget/static/js/runtime~main.bb354097.js"
+  }
+]);
