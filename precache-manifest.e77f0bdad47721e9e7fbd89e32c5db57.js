6f1fd880ab3d2aee2a33fbf6d69df9adf0f6da2f
hgupta10 <himanshu.h.gupta@capgemini.com>
2019-10-20 01:28:42 +0530

    Updates

---
+++ precache-manifest.e77f0bdad47721e9e7fbd89e32c5db57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3134cd99f1f8444e2873a60ea1c441",
+    "url": "/cart_widget/index.html"
+  },
+  {
+    "revision": "08d9e2f46ca583a6d1bf",
+    "url": "/cart_widget/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2123b3ed9e55e4d8e2a9",
+    "url": "/cart_widget/static/css/main.c77b1aac.chunk.css"
+  },
+  {
+    "revision": "08d9e2f46ca583a6d1bf",
+    "url": "/cart_widget/static/js/2.f6ce2c42.chunk.js"
+  },
+  {
+    "revision": "2123b3ed9e55e4d8e2a9",
+    "url": "/cart_widget/static/js/main.4f18d772.chunk.js"
+  },
+  {
+    "revision": "68f743b7735fc31ad078",
+    "url": "/cart_widget/static/js/runtime~main.bb354097.js"
+  }
+]);
